eead2b8da8ff487679b7fded53a42a2cf02b392d
Mohit Kumar <mks.djcet@gmail.com>
2026-08-08 09:50:10 +0530

    Updated code

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-from flask import Flask
-from uuid import UUID
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Home Page"
-
-@app.route("/user/<string:name>")
-def user(name):
-    return f"User ID: {name}"
-
-@app.route("/price/<float:amount>")
-def price(amount):
-    return f"Price: {amount}"
-
-@app.route("/files/<path:file_path>")
-def files(file_path):
-    return file_path
-
-@app.route("/student/<uuid:user_id>")
-def student(user_id):
-    return str(user_id)
+@app.route("/search")
+def search():
+    name = request.args.get("name","Guest")
+    course = request.args.get("course","UnKnown")
+    return f"{name} is learning {course}"
 
 if __name__ == "__main__":
     app.run(debug=True)
