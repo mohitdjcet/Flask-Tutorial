41682c08ef227149e49957899640452982e2f7fa
Mohit Kumar <mks.djcet@gmail.com>
2026-08-12 16:08:49 +0530

    Updated code

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/search")
-def search():
-    name = request.args.get("name","Guest")
-    course = request.args.get("course","UnKnown")
-    return f"{name} is learning {course}"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
