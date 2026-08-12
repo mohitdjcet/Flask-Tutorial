7c515240da9bb5b914f7ea0116c231d275f95de2
Mohit Kumar <mks.djcet@gmail.com>
2026-08-12 16:47:15 +0530

    Updated code

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    name = "Rohit"
-    course = "Flask"
+    first_name = "Rohit"
+    last_name = "Kumar"
     city = "Delhi"
-    age:28
+    price=500
+    course= [
+        "python",
+        "Flask",
+        "Django",
+        "FastApi"
+    ]
     return render_template("index.html", 
-                           name=name,
-                           course=course,
-                           city=city,
-                           age=28)
+                        first_name=first_name,
+                        last_name = last_name,
+                        course=course,
+                        city=city,
+                        price=price)
 
 if __name__ == "__main__":
     app.run(debug=True)
