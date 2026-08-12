b6a9e9c37168a274bcf9f25e1d50c5fc6f11ce5c
Mohit Kumar <mks.djcet@gmail.com>
2026-08-12 22:57:40 +0530

    Updated code

--- app.py
+++ app.py
@@ -4,18 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    courses = [
-        "Python",
-        "Flask",
-        "React",
-        "Django"
-    ]
-    student = {
-        "name":"Mohit",
-        "course":"Flask",
-        "city":"Delhi"
-    }
-    return render_template("index.html",courses=courses,student=student)
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
