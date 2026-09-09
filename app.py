8f504545b66a35bd91842bc892c898194cb925f9
Mohit Kumar <mks.djcet@gmail.com>
2026-09-09 13:22:22 +0530

    Updated code

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 app.secret_key = "change-this-in-prod"
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"),500
+
 @app.route("/login")
 def login():
     session["username"]= "Mohit"
