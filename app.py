3e061aac501469a90a3606ce7a16005672c8ebd4
Mohit Kumar <mks.djcet@gmail.com>
2026-09-16 13:27:56 +0530

    Updated code

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///app.db"
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),nullable= False)
+    email= db.Column(db.String(120),nullable= False)
+    age= db.Column(db.Integer)
+
 @app.route("/")
 def home():
     return "Flask + SQLite + SQLAlchemy is working !"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
