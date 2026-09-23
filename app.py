dad9ddd1f13c3dbd30b23f02095097b6e0b4cedb
Mohit Kumar <mks.djcet@gmail.com>
2026-09-23 20:37:05 +0530

    Updated code

--- app.py
+++ app.py
@@ -1,105 +1,41 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///app.db"
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"mysql+pymysql://"
+    f"{DB_USERNAME}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_URL
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100),nullable= False)
-    email= db.Column(db.String(120),nullable= False)
-    age= db.Column(db.Integer)
-
 @app.route("/")
 def home():
-    return "Flask + SQLite + SQLAlchemy is working !"
+    return "Flask application is running !"
 
-@app.route("/create-user", methods=["POST"])
-def create_user():
-    data = request.get_json()
-
-    name = data.get("name")
-    email = data.get("email")
-    age = data.get("age")
-
-    user = User(
-        name=name,
-        email=email,
-        age=age
+@app.route("/db-test")
+def db_test():
+    result = db.session.execute(
+        text("SELECT 1")
     )
-
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({
-        "message":"User Created Successfully !",
-        "user_id":user.id
-    })
-
-@app.route("/get-user", methods=["GET"])
-def get_users():
-    users = User.query.all()
-    users_data = []
-    for user in users:
-        users_data.append({
-            "id":user.id,
-            "name":user.name,
-            "email":user.email,
-            "age":user.age
-        })
-    return jsonify(users_data)
-
-@app.route("/users1")
-def users():
-    users = User.query.all()
-    return render_template("user.html",users=users)
-
-@app.route("/users/<int:id>", methods=["GET"])
-def get_user(id):
-    user = db.get_or_404(User,id)
-    return jsonify({
-        "id":user.id,
-        "name":user.name,
-        "email":user.email,
-        "age":user.age
-    })
-
-
-@app.route("/users/<int:id>", methods=["PATCH"])
-def updated_user(id):
-    data = request.get_json()
-    user = db.get_or_404(User,id)
-    if "name" in data:
-        user.name = data["name"]
-    if "email" in data:
-            user.email = data["email"]
-    if "age" in data:
-            user.age = data["age"]
-    db.session.commit()
-    return jsonify({
-         "message":"User Updated Successfully !",
-         "user":{
-              "id":user.id,
-              "name":user.name,
-              "email":user.email,
-              "age":user.age
-         }
-    })
-
-@app.route("/users/<int:id>", methods=["DELETE"])
-def delete_user(id):
-     user = db.get_or_404(User,id)
-     db.session.delete(user)
-     db.session.commit()
-
-     return jsonify({
-          "message":"User deleted Successfully"
-     })
+    value = result.scalar()
+    return f"MySQL connected Fine ! Result : {value}"
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
